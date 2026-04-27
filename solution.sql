76e40310b1e51d6f8edeaf0cd0534dde49058d8c
sanjyotsachdev-aka <sanjyotsachdev@gmail.com>
2026-04-27 15:12:42 -0400

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,5 +1,3 @@
-SELECT DATE_FORMAT(PaymentDate, '%Y/%m') AS Month,
-       SUM(Amount) AS TotalRevenue
-FROM Payments
-GROUP BY DATE_FORMAT(PaymentDate, '%Y/%m')
-ORDER BY Month;
+SELECT * FROM CLASSES;
+SELECT * FROM MembershipPlans;
+SELECT * FROM Payments;
