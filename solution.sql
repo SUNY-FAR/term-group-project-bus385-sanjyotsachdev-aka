5f3257cac563012caa63d0b0f6c1b1a42fcad00c
sanjyotsachdev-aka <sanjyotsachdev@gmail.com>
2026-04-27 14:19:13 -0400

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,2 +1,32 @@
 -- Write your SQL query below:
-SELECT * FROM classes;
+SELECT 
+    c.ClassID,
+    c.ClassName,
+    c.Instructor,
+    c.ScheduleDateTime,
+    m.MemberID,
+    m.FirstName,
+    m.LastName
+FROM Classes c
+LEFT JOIN ClassRegistrations cr ON c.ClassID = cr.ClassID
+LEFT JOIN Members m ON cr.MemberID = m.MemberID
+WHERE c.ClassName = 'Zumba'
+ORDER BY c.ClassID;
+
+SELECT 
+    DATE_FORMAT(PaymentDate, '%Y/%m') AS Month,
+    SUM(Amount) AS total_revenue
+FROM Payments
+GROUP BY DATE_FORMAT(PaymentDate, '%Y/%m')
+ORDER BY Month;
+
+SELECT 
+    DATE_FORMAT(p.PaymentDate, '%Y/%m') AS Month,
+    mp.PlanName AS plan_name,
+    SUM(p.Amount) AS total_revenue
+FROM Payments p
+JOIN Members m ON p.MemberID = m.MemberID
+JOIN MembershipPlans mp ON m.MembershipPlanID = mp.MembershipPlanID
+WHERE DATE_FORMAT(p.PaymentDate, '%Y/%m') = '2026/04'
+GROUP BY DATE_FORMAT(p.PaymentDate, '%Y/%m'), mp.PlanName
+ORDER BY Month, total_revenue DESC;
